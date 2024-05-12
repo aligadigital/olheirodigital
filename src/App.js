db652c6d6a2f974c8bea0138702d9cd80ed71e3a
fagneracr <fagneracr@gmail.com>
2024-05-12 14:47:44 -0300

    change path

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 <div className="App">
 <BrowserRouter>
   <Routes>
-    <Route path="/login" element={<LoginForm />} />
+    <Route path="/" element={<LoginForm />} />
   </Routes>
 </BrowserRouter>
 </div>
