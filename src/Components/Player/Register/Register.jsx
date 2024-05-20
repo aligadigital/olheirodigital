197888a0f507f5afa6c775b6397c6b78a8f9eef9
fagneracr <fagneracr@gmail.com>
2024-05-20 08:48:10 -0300

    send form.

--- src/Components/Player/Register/Register.jsx
+++ src/Components/Player/Register/Register.jsx
@@ -12,37 +12,94 @@ function PlayerRegister()  {
     };
     return (
     <>
-       <div className='wrapper-cadastro'>
-        <div className="title">CADASTRO</div>
-        <div className='form-box-cadastro'>
-            <form onSubmit={handleSubmit}>
-               <div className='col2'>
-               <div className='input-box-register'>
-                    <input type='text' name='nome' placeholder='nome'/>
-                    <label className='place'>Nome</label>
-               </div>
-               <div className='input-box-register'>
-                    <input type='text' name='sobrenome' placeholder='nome'/>
-                    <label className='place'>Sobrenome</label>
-               </div>
-               </div>
-               <div className='col3'>
-               <div className='input-box-register'>
-                    <input type='text' name='email' placeholder='nome'/>
-                    <label className='place'>email</label>
-               </div>
-               <div className='input-box-register'>
-                    <input type='text' name='CPF' placeholder='nome'/>
-                    <label className='place'>CPF</label>
-               </div>
-               <div className='input-box-register'>
-                    <input type='text' name='RG' placeholder='nome'/>
-                    <label className='place'>RG</label>
-               </div>
-               </div>
-            </form>
-        </div>
-       </div>
+    <div className="wrapper-cadastro">
+      <div className="title">CADASTRO</div>
+      <div className="form-box-cadastro">
+        <form onSubmit={handleSubmit}>
+          <div className="input-row">
+            <div className="input-box-register">
+              <input type="text" name="nome" placeholder="nome" />
+              <label className="place">Nome</label>
+            </div>
+            <div className="input-box-register">
+              <input type="text" name="sobrenome" placeholder="sobrenome" />
+              <label className="place">Sobrenome</label>
+            </div>
+          </div>
+          <div className="input-row">
+            <div className="input-box-register">
+              <input type="text" name="email" placeholder="email" />
+              <label className="place">email</label>
+            </div>
+            <div className="input-box-register">
+              <input type="text" name="CPF" placeholder="CPF" />
+              <label className="place">CPF</label>
+            </div>
+
+          </div>
+          <div className="input-row">
+          <div className="input-box-register medium-input">
+              <input type="text" name="DtNasc" placeholder="Data Nascimento" />
+              <label className="place">Data Nascimento</label>
+            </div>
+            </div>
+          <div className="input-row">
+            <div className="input-box-register">
+              <input type="text" name="Rua" placeholder="Rua" />
+              <label className="place">Rua</label>
+            </div>
+            <div className="input-box-register short-input">
+              <input type="text" name="NumeroRua" placeholder="Nro"  />
+              <label className="place">Nro</label>
+            </div>
+            </div>
+          <div className="input-row">
+          <div className="input-box-register medium-input">
+              <input type="text" name="CEP" placeholder="CEP" />
+              <label className="place">CEP</label>
+            </div>
+            <div className="input-box-register">
+              <input type="text" name="Cidade" placeholder="Cidade" />
+              <label className="place">Cidade</label>
+            </div>
+            
+          </div>
+          <div className="input-row">
+          <div className="input-box-register">
+              <input type="text" name="Bairro" placeholder="Bairro" />
+              <label className="place">Bairro</label>
+            </div>
+            <div className="input-box-register short-input">
+              <input type="text" name="Estado" placeholder="Estado" />
+              <label className="place">Estado</label>
+            </div>
+        
+            <div className="input-box-register  medium-input">
+              <input type="text" name="Telefone" placeholder="Telefone" />
+              <label className="place">Telefone</label>
+            </div>
+          </div>
+          <div className="input-row">
+               <div className="input-box-register">
+              <input type="text" name="Senha" placeholder="Senha" />
+              <label className="place">Senha</label>
+            </div>
+            </div>
+          <div className="input-row">
+            <div className="input-box-register">
+              <input type="text" name="Senha1" placeholder="Digite novamente a senha" />
+              <label className="place">Digite novamente a senha</label>
+            </div>
+          </div>
+          <div className="input-row">
+            <div className="input-box-register">
+              <input type="checkbox" name="Termo" placeholder="Termo" />
+              <label >Declaro que li os termos de uso do usuário</label>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
 
     </>
     );
