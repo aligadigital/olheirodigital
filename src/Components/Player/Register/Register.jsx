53166584bd12676457ff7ba7892429aa658b55de
fagneracr <fagneracr@gmail.com>
2024-05-22 08:48:03 -0300

    change form

--- src/Components/Player/Register/Register.jsx
+++ src/Components/Player/Register/Register.jsx
@@ -2,6 +2,7 @@ import './Register.css';
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import InputMask from 'react-input-mask';
 
 function PlayerRegister()  {
     const navigate = useNavigate();
@@ -18,85 +19,100 @@ function PlayerRegister()  {
         <form onSubmit={handleSubmit}>
           <div className="input-row">
             <div className="input-box-register">
-              <input type="text" name="nome" placeholder="nome" />
-              <label className="place">Nome</label>
+              <input type="text" id="name" name="nome" placeholder="nome" />
+              <label className="place" htmlFor='name'>Nome</label>
             </div>
             <div className="input-box-register">
-              <input type="text" name="sobrenome" placeholder="sobrenome" />
-              <label className="place">Sobrenome</label>
+              <input type="text" name="sobrenome" placeholder="sobrenome" id="sobrenome" />
+              <label className="place" htmlFor='sobrenome'>Sobrenome</label>
             </div>
           </div>
           <div className="input-row">
             <div className="input-box-register">
-              <input type="text" name="email" placeholder="email" />
-              <label className="place">email</label>
+              <input type="text" name="email" placeholder="email" id="email"/>
+              <label className="place" htmlFor='email'>email</label>
             </div>
             <div className="input-box-register">
-              <input type="text" name="CPF" placeholder="CPF" />
-              <label className="place">CPF</label>
+              <input type="text" id="cpf" name="CPF" placeholder="CPF" />
+              <label className="place" htmlFor='cpf'>CPF</label>
             </div>
 
           </div>
           <div className="input-row">
           <div className="input-box-register medium-input">
-              <input type="text" name="DtNasc" placeholder="Data Nascimento" />
-              <label className="place">Data Nascimento</label>
+
+             <InputMask
+                mask="99/99/9999"
+                placeholder="Data Nascimento"
+                name="DtNasc"
+                id='dtnasc'
+                maskChar={null}
+              >
+                {(inputProps) => <input {...inputProps} type="text" />}
+              </InputMask>
+              <label className="place" htmlFor='dtnasc'>Data Nascimento</label>
             </div>
             </div>
           <div className="input-row">
             <div className="input-box-register">
-              <input type="text" name="Rua" placeholder="Rua" />
-              <label className="place">Rua</label>
+              <input type="text" name="Rua" placeholder="Rua" id="rua" />
+              <label className="place" htmlFor='rua'>Rua</label>
             </div>
             <div className="input-box-register short-input">
-              <input type="text" name="NumeroRua" placeholder="Nro"  />
-              <label className="place">Nro</label>
+              <input type="text" name="NumeroRua" placeholder="Numero" id='nrcasa' />
+              <label className="place" htmlFor='nrcasa'>Numero</label>
             </div>
             </div>
           <div className="input-row">
           <div className="input-box-register medium-input">
-              <input type="text" name="CEP" placeholder="CEP" />
-              <label className="place">CEP</label>
+              <input type="text" name="CEP" placeholder="CEP" id="cep" />
+              <label className="place" htmlFor='cep'>CEP</label>
             </div>
             <div className="input-box-register">
-              <input type="text" name="Cidade" placeholder="Cidade" />
-              <label className="place">Cidade</label>
+              <input type="text" name="Cidade" placeholder="Cidade" id="cidade"/>
+              <label className="place" htmlFor='cidade'>Cidade</label>
             </div>
             
           </div>
           <div className="input-row">
           <div className="input-box-register">
-              <input type="text" name="Bairro" placeholder="Bairro" />
-              <label className="place">Bairro</label>
+              <input type="text" name="Bairro" placeholder="Bairro" id="bairro"/>
+              <label className="place" htmlFor='bairro'>Bairro</label>
             </div>
             <div className="input-box-register short-input">
-              <input type="text" name="Estado" placeholder="Estado" />
-              <label className="place">Estado</label>
+              <input type="text" name="Estado" placeholder="Estado" id="estado"/>
+              <label className="place" htmlFor='estado'>Estado</label>
             </div>
         
             <div className="input-box-register  medium-input">
-              <input type="text" name="Telefone" placeholder="Telefone" />
-              <label className="place">Telefone</label>
+              <input type="text" name="Telefone" placeholder="Telefone" id="telefone" />
+              <label className="place" htmlFor='telefone'>Telefone</label>
             </div>
           </div>
           <div className="input-row">
                <div className="input-box-register">
-              <input type="text" name="Senha" placeholder="Senha" />
-              <label className="place">Senha</label>
+              <input type="text" name="Senha" placeholder="Senha" id="senha" />
+              <label className="place" htmlFor='senha'>Senha</label>
             </div>
             </div>
           <div className="input-row">
             <div className="input-box-register">
-              <input type="text" name="Senha1" placeholder="Digite novamente a senha" />
-              <label className="place">Digite novamente a senha</label>
+              <input type="text" name="Senha1" placeholder="Digite novamente a senha" id="senha1"/>
+              <label className="place" htmlFor='senha1'>Digite novamente a senha</label>
             </div>
           </div>
-          <div className="input-row">
             <div className="input-box-register">
-              <input type="checkbox" name="Termo" placeholder="Termo" />
-              <label >Declaro que li os termos de uso do usuário</label>
-            </div>
+            <label >
+              <input type="checkbox" name="Termo" placeholder="Termo"/>
+              Declaro que li os termos de uso do usuário
+            </label>
           </div>
+          <div className="input-box-register">
+          <div className="input-button">
+          <button type='submit'>CADASTRAR</button>
+          </div>
+      </div>
+        
         </form>
       </div>
     </div>
